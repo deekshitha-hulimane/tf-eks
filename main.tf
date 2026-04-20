3b7f7e579cede0755b8a301eed8a25a0576af897
deekshitha-hulimane <deekshithahs9@gmail.com>
2026-04-20 15:37:02 +0530

    Fix terraform tool naming and post block

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-eks-cluster-v2"
+  cluster_name    = "my-eks-cluster"
   cluster_version = "1.29"
 
   vpc_id     = aws_vpc.main.id
